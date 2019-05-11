aa1c2d42f5508a7bfe904eb49aa3a1db791c07a6
nastya1204 <tan98-52@yandex.ru>
2019-05-11 14:13:49 +0300

    style fix

--- modules/task_3/tuzhilkina_a_filter_gauss_task3_tbb/main.cpp
+++ modules/task_3/tuzhilkina_a_filter_gauss_task3_tbb/main.cpp
@@ -10,34 +10,34 @@
 #include "tbb/task_scheduler_init.h"
 
 void InitMatr(int rows, int cols, int** m) {
-	for (int i = 0; i < rows; i++)
-		for (int j = 0; j < cols; j++)
-			m[i][j] = static_cast<int>(std::rand() % 255);
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < cols; j++)
+            m[i][j] = static_cast<int>(std::rand() % 255);
 }
 
 
 void InitKern(double kernel[3][3], int radius, double sigma) {
-	double norm = 0;
+    double norm = 0;
 
-	for (int i = -radius; i <= radius; i++)
-		for (int j = -radius; j <= radius; j++) {
-			kernel[i + radius][j + radius] = (exp(-(i * i + j * j) / (2 * sigma * sigma)));
-			norm += kernel[i + radius][j + radius];
-		}
+    for (int i = -radius; i <= radius; i++)
+        for (int j = -radius; j <= radius; j++) {
+            kernel[i + radius][j + radius] = (exp(-(i * i + j * j) / (2 * sigma * sigma)));
+            norm += kernel[i + radius][j + radius];
+        }
 
-	for (int i = 0; i < 3; i++)
-		for (int j = 0; j < 3; j++)
-			kernel[i][j] /= norm;
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            kernel[i][j] /= norm;
 }
 
 inline int Clamp(int value, int min, int max) {
-	if (value < min)
-		return min;
+    if (value < min)
+        return min;
 
-	if (value > max)
-		return max;
+    if (value > max)
+        return max;
 
-	return value;
+    return value;
 }
 
 void par_mat(double kernel[3][3], int **picture1, int **picture2, int j, size_t size_rows, size_t size_cols) {
@@ -65,36 +65,36 @@ void parallel_matrix_multiply(double kernel[3][3], int **picture1, int **picture
 }
 
 int main() {
-	int rows, cols;
-	double st, end;
-	double kernel[3][3];
+    int rows, cols;
+    double st, end;
+    double kernel[3][3];
 
-	rows = 5000, cols = 5000;
+    rows = 5000, cols = 5000;
 
-	int** picture = new int*[rows];
-	int** res_tbb = new int*[rows];
+    int** picture = new int*[rows];
+    int** res_tbb = new int*[rows];
 
-	for (int i = 0; i < rows; i++) {
-		picture[i] = new int[cols];
-		res_tbb[i] = new int[cols];
-	}
+    for (int i = 0; i < rows; i++) {
+        picture[i] = new int[cols];
+        res_tbb[i] = new int[cols];
+    }
 
-	InitMatr(rows, cols, picture);
-	InitKern(kernel, 1, 1.0);
+    InitMatr(rows, cols, picture);
+    InitKern(kernel, 1, 1.0);
 
-	st = omp_get_wtime();
-	tbb::task_scheduler_init init(4);
-	parallel_matrix_multiply(kernel, picture, res_tbb, rows, cols);
-	end = omp_get_wtime();
-	std::cout << "Time par TBB:" << end - st << std::endl;
+    st = omp_get_wtime();
+    tbb::task_scheduler_init init(4);
+    parallel_matrix_multiply(kernel, picture, res_tbb, rows, cols);
+    end = omp_get_wtime();
+    std::cout << "Time par TBB:" << end - st << std::endl;
 
-	for (int i = 0; i < rows; i++) {
-		delete[] picture[i];
-		delete[] res_tbb[i];
-	}
+    for (int i = 0; i < rows; i++) {
+        delete[] picture[i];
+        delete[] res_tbb[i];
+    }
 
-	delete[] picture;
-	delete[] res_tbb;
+    delete[] picture;
+    delete[] res_tbb;
 
-	return 0;
+    return 0;
 }
